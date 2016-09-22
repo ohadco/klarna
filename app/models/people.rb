baead3d3f02f2967fc87ed37e2aac85dc66d1557
Ohad Cohen <ohad66@gmail.com>
2016-09-22 15:29:22 +0300

    Comment people model

--- app/models/people.rb
+++ app/models/people.rb
@@ -1,27 +1,38 @@
 class People < ActiveRecord::Base
 
-#-- validations --#
+  # MAX_AMOUNT_TO_SHOW configures the maximum..
+  # ..amount of people to show in each batch
+  MAX_AMOUNT_TO_SHOW = 100
 
-  # validates that that the records are not duplicated
+#-- validations --#
+  # validates that the imported records are not duplicated
+  # if we will use more than one data source for import - we..
+  # ..might need to remove this validation
   validates_uniqueness_of :uid
 
 #-- scopes --#
-  scope :with_name, ->(name) { where('name like ?', "%#{name}%") }
+  # using lower case in order to find all sort of queries
+  scope :with_name, ->(name) { where('LOWER(name) like ?', "%#{name.downcase}%") }
+
+  # with_phone allows searching from the some part of the phone number
+  # if it doesn't needed - need to remove the first % from the search and create an index
   scope :with_phone, ->(phone) { where('phone like ?', "%#{phone}%") }
+  scope :after_id, ->(id) { where('id > ', id) }
   scope :of_age, ->(age) {
     where('birthday >= ? AND birthday < ?',
-    (age+1).years.ago.to_date,
-    (age).years.ago.to_date
-        ) }
+      (age+1).years.ago.to_date,
+      (age).years.ago.to_date
+    ) }
 
-#-- methods --#
-
-  # returns person's age
+#-- public methods --#
+  # @return person's age
   def age
     Date.today.year - birthday.year
   end
 
-  # returns person's address
+  # @return person's address
+  # assumes all [street, city, country] is not nil
+  # if that can't be assumed, this method should return other text
   def address
     "#{street} #{city}, #{country}."
   end
